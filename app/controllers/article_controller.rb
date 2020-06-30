71f6aaf6f26ccc360bf5c088714d2050c7720a73
Jamie Macey <jamie.git@tracefunc.com>
2020-06-29 22:05:44 -0700

    Import/stub out remaining non-Devise controllers+views

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,10 @@
+class ArticleController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'home#index'
+
+  # Real World implementation note:
+  # The Rails Way would be to have our routes be RESTful, like so:
+  # resources :articles # includes editor actions
+  # resources :profiles
+  # resource :settings
+
+  # Devise TODO
+  # get '/login'
+  # get '/register'
+  get '/settings', to: 'settings#edit'
+  get '/editor', to: 'article#new'
+  get '/editor/:id', to: 'article#edit'
+  get '/article/:id', to: 'article#show'
+  get '/profile/:id', to: 'profile#show'
+  # get '/profile/:id/favorites', to: TODO
 end
