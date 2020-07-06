667092662dcbcd7d2d399e13658e3ab3eb02947a
Jamie Macey <jamie.git@tracefunc.com>
2020-07-05 21:08:54 -0700

    Article model, sample system test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_035828) do
+ActiveRecord::Schema.define(version: 2020_07_06_030917) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.string "description"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slug"], name: "index_articles_on_slug"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_07_01_035828) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "articles", "users"
 end
---
+++ test/system/articles_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+ 
+class ArticlesTest < ApplicationSystemTestCase
+  test "view home page" do
+    visit '/'
+    assert_selector "h1", text: "How to build webapps that scale"
+  end
+
+  test "viewing a article" do
+    visit '/article/webapps'
+    assert_selector "h1", text: "How to build webapps that scale"
+  end
+end
